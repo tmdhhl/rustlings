59bd00eab7f2c29525884a1330bbfb972f224aad
tmdhhl <tmdhhl@gmail.com>
2024-05-10 23:27:40 +0800

    done

---
+++ exercises/22_clippy/target/debug/deps/clippy3-30bce5d28d5a6e37.d
@@ -0,0 +1,9 @@
+/Users/xulei/projects/rustlings/exercises/22_clippy/target/debug/deps/libclippy3-30bce5d28d5a6e37.rmeta: clippy3.rs Cargo.toml
+
+/Users/xulei/projects/rustlings/exercises/22_clippy/target/debug/deps/clippy3-30bce5d28d5a6e37.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
